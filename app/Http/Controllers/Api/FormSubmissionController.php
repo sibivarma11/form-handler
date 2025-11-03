d1eb4bd36ea274b0aa0d09c7ba68d9887f80ab79
sibivarma11 <sibivarma11@gmail.com>
2025-11-03 12:12:15 +0530

    Add email configuration option to manage form submission notifications

--- app/Http/Controllers/Api/FormSubmissionController.php
+++ app/Http/Controllers/Api/FormSubmissionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\FormSubmission;
+use App\Models\EmailSetting;
 use App\Mail\FormSubmissionNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -35,10 +36,23 @@ class FormSubmissionController extends Controller
         ]);
 
         try {
-            Mail::to(config('mail.from.address'))
-                ->send(new FormSubmissionNotification($submission));
+            $recipient = EmailSetting::get('contact_form_recipient');
+            
+            if ($recipient) {
+                config([
+                    'mail.mailers.smtp.host' => EmailSetting::get('mail_host'),
+                    'mail.mailers.smtp.port' => EmailSetting::get('mail_port'),
+                    'mail.mailers.smtp.username' => EmailSetting::get('mail_username'),
+                    'mail.mailers.smtp.password' => EmailSetting::get('mail_password'),
+                    'mail.mailers.smtp.encryption' => EmailSetting::get('mail_encryption'),
+                    'mail.from.address' => EmailSetting::get('mail_from_address'),
+                    'mail.from.name' => EmailSetting::get('mail_from_name'),
+                ]);
+                
+                Mail::to($recipient)->send(new FormSubmissionNotification($submission));
+            }
         } catch (\Exception $e) {
-            \Log::error('Failed to send form notification: ' . $e->getMessage());
+            // Email sending failed silently
         }
 
         return response()->json([
